93bb3041fdf10b3a3d0068c9db560939cc60903a
Rasmus Bondesson <raek@raek.se>
2020-09-19 00:04:37 +0200

    Replace "quad" with "parts"

--- pladder/snusk.py
+++ pladder/snusk.py
@@ -12,7 +12,6 @@ class SnuskDb:
         self._prep_db_file_path = prep_db_file_path
         with open(self._prep_db_file_path, "rt", encoding="utf8") as f:
             self._prep_entries = json.load(f)
-        
 
     def _save(self):
         with open(self._db_file_path, "wt", encoding="utf8") as f:
@@ -22,39 +21,48 @@ class SnuskDb:
             json.dump(self._prep_entries, f, ensure_ascii=False, indent=True, sort_keys=True)
 
     def snusk(self):
-        return self._format_quad(self._random_quad())
+        return self._format_parts(self._random_parts())
 
     def directed_snusk(self, target):
-        quad = self._random_quad()
-        i = random.choice([0, 1, 2, 3])
-        if i % 2 == 0:
+        parts = self._random_parts()
+        i = random.choice([0, 1, 3, 4])
+        if i in [0, 3]:
             part = target
         else:
             part = target + "ern"
-        quad[i] = part
-        return self._format_quad(quad)
+        parts[i] = part
+        return self._format_parts(parts)
 
     def example_snusk(self, a, b):
-        quad = self._random_quad()
+        parts = self._random_parts()
         if random.choice([False, True]):
-            quad[0] = a
-            quad[3] = b
+            parts[0] = a
+            parts[4] = b
         else:
-            quad[1] = b
-            quad[2] = a
-        return self._format_quad(quad)
+            parts[1] = b
+            parts[3] = a
+        return self._format_parts(parts)
 
-    def _format_quad(self, quad):
-            prep = random.choice(self._prep_entries)
-            quad.insert(2, prep) #Insert the preposition into the quad which technically makes it a pent, but who keeps score? 
-            return "{}{} {} {}{}".format(*quad)
-            
+    def _format_parts(self, parts):
+        return "{}{} {} {}{}".format(*parts)
 
-    def _random_quad(self):
-        return [self._random_entry()[i % 2] for i in range(4)]
+    def _random_parts(self):
+        return [
+            self._random_a_entry(),
+            self._random_b_entry(),
+            self._random_prep(),
+            self._random_a_entry(),
+            self._random_b_entry(),
+        ]
 
-    def _random_entry(self):
-        return random.choice(self._entries)
+    def _random_a_entry(self):
+        return random.choice(self._entries)[0]
+
+    def _random_b_entry(self):
+        return random.choice(self._entries)[1]
+
+    def _random_prep(self):
+        return random.choice(self._prep_entries)
 
     def add_snusk(self, a, b):
         new_entry = [a, b]
