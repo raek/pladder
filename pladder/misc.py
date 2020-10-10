83df9cc89d714c843176e9a17685fe2db0a2975f
Emil Nielsen <emil.nielsen@gmail.com>
2020-10-10 14:41:17 +0200

    Covering some more corner cases for kloofify function

--- pladder/misc.py
+++ pladder/misc.py
@@ -1,39 +1,55 @@
 import re
 
 #TODO: Replace multiples of same consonants with single consonant (if between oo and y)
-#TODO: Replace all vowels with oo in the vowel-consonant(-vowel) ender case
 
 class MiscCmds():
+    def strip_double_consonants(self, target):
+        r = re.compile('oo(bb|cc|ck|dd|ff|gg|hh|jj|kk|ll|mm|nn|pp|qq|rr|ss|tt|vv|ww|xx|zz)y$', re.IGNORECASE)
+        m = r.search(target)
+        if m:
+            return target[0:m.start()+2:] + target[m.end()-2:len(target):]
+        else:
+            return target
+
+    def strip_vowels_from_end(self, target):
+        r = re.compile('[^aeiouyåäö][aeiouyåäö]+$', re.IGNORECASE)
+        m = r.search(target)
+        if m:
+            return target[0:m.start()+1:] + target[len(target)-1]
+        else:
+            return target
+        
+        
     def kloofify_word(self, target):
         # String ends with vowels, but has vowel(s)-consonant before that
-        w1 = re.compile('[aeiouyåäö]+[^aeiouyåäö]+[aeiouyåäö]+$', re.IGNORECASE)
+        r1 = re.compile('[aeiouyåäö]+[^aeiouyåäö]+[aeiouyåäö]+$', re.IGNORECASE)
         # Same as previous, but only checks for a single preceding vowel
-        w1s = re.compile('[aeiouyåäö][^aeiouyåäö]+[aeiouyåäö]+$', re.IGNORECASE)
+        r1s = re.compile('[aeiouyåäö][^aeiouyåäö]+[aeiouyåäö]+$', re.IGNORECASE)
         # String ends with consonants, but there are preceding vowels
-        w2 = re.compile('[aeiouyåäö]+[^aeiouyåäö]+$', re.IGNORECASE)
+        r2 = re.compile('[aeiouyåäö]+[^aeiouyåäö]+$', re.IGNORECASE)
         # Same as previous, but only checks for a single preceding vowel
-        w2s = re.compile('[aeiouyåäö][^aeiouyåäö]+$', re.IGNORECASE)
+        r2s = re.compile('[aeiouyåäö][^aeiouyåäö]+$', re.IGNORECASE)
         # String ends with vowels, with preceding consonant
-        w3 = re.compile('[^aeiouyåäö][aeiouyåäö]+$', re.IGNORECASE)
+        r3 = re.compile('[^aeiouyåäö][aeiouyåäö]+$', re.IGNORECASE)
         
-        s1 = w1.search(target)
-        s1s = w1s.search(target)
-        if s1:
-            return target[0:s1.start():] + "oo" + target[s1s.start()+1:s1.end()-1:] + "y"
+        m1 = r1.search(target)
+        m1s = r1s.search(target)
+        if m1:
+            return self.strip_vowels_from_end(target[0:m1.start():] + "oo" + target[m1s.start()+1:m1.end()-1:] + "y")
         
-        s2 = w2.search(target)
-        s2s = w2s.search(target)
-        if s2:
-            return target[0:s2.start():] + "oo" + target[s2s.start()+1:s2.end():] + "y"
+        m2 = r2.search(target)
+        m2s = r2s.search(target)
+        if m2:
+            return target[0:m2.start():] + "oo" + target[m2s.start()+1:m2.end():] + "y"
         
-        s3 = w3.search(target)
-        if s3:
-            return target[0:s3.start()+1:] + "ooey"
+        m3 = r3.search(target)
+        if m3:
+            return target[0:m3.start()+1:] + "ooey"
         else:
             return target + "ooey"
     
     def kloofify(self, target):
         new_str = ""
         for word in target.split(' '):
-            new_str = new_str + self.kloofify_word(word) + " "
+            new_str = new_str + self.strip_double_consonants(self.kloofify_word(word)) + " "
         return new_str.rstrip()
