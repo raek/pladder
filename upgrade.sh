675f60b0fb6b28a9c60dce805803b2f736f6e769
Rasmus Bondesson <raek@raek.se>
2020-10-14 20:55:24 +0200

    Tweak upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -11,6 +11,6 @@ fi
 git fetch --prune origin
 git merge --ff-only origin/master
 pip3 uninstall -y pladder
-pip3 install "$1"
+pip3 install --user --no-warn-script-location "$1"
 systemctl --user daemon-reload
 systemctl --user restart pladder-bot.service
