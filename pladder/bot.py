d2f7ceec4c2cb9aa0b1ab5279a433ce872fbfc0b
Rasmus Bondesson <raek@raek.se>
2020-10-25 15:57:06 +0100

    Integrate pladder.script into pladder.bot

--- pladder/bot.py
+++ pladder/bot.py
@@ -6,6 +6,7 @@ import os
 import re
 
 from pladder import LAST_COMMIT
+from pladder.script import ScriptError, ApplyError, CommandBinding, interpret, lookup_command, apply_call
 
 
 def main():
@@ -22,31 +23,6 @@ def main():
         bus.publish("se.raek.PladderBot", bot)
         loop = GLib.MainLoop()
         loop.run()
-
-
-class Command(namedtuple("Command", "name, fn, varargs, regex, contextual")):
-    @property
-    def display_name(self):
-        if self.regex:
-            return f"/{self.name.pattern[1:-1]}/"
-        else:
-            return self.name
-
-    @property
-    def usage(self):
-        result = self.display_name
-        parameters = list(signature(self.fn).parameters.values())
-        if self.contextual:
-            # pop context argument
-            parameters.pop(0)
-        for i, parameter in enumerate(parameters):
-            if i == len(parameters) - 1 and self.varargs:
-                result += f" {{{parameter.name}...}}"
-            elif parameter.default != Parameter.empty:
-                result += f" [{parameter.name}]"
-            else:
-                result += f" <{parameter.name}>"
-        return result
 
 
 class PladderBot(ExitStack):
@@ -69,7 +45,7 @@ class PladderBot(ExitStack):
         super().__init__()
         os.makedirs(state_dir, exist_ok=True)
         self.state_dir = state_dir
-        self.commands = []
+        self.bindings = []
         self.register_command("help", self.help)
         self.register_command("version", self.version)
 
@@ -79,80 +55,58 @@ class PladderBot(ExitStack):
                    'channel': channel,
                    'nick': nick,
                    'text': text}
-        return self.interpret(context, text)
-
-    def register_command(self, name, fn, varargs=False, regex=False, contextual=False):
-        if regex:
-            name = re.compile("^" + name + "$")
-        self.commands.append(Command(name, fn, varargs, regex, contextual))
+        try:
+            return interpret(self.bindings, context, text)
+        except ApplyError as e:
+            return "Usage: {}".format(self.command_usage(e.command))
+        except ScriptError as e:
+            return str(e)
 
     def interpret(self, context, text):
-        words = self.eval(text)
-        return self.apply(context, words)
-
-    def eval(self, text):
-        return text.split()
+        return interpret(self.bindings, context, text)
 
     def apply(self, context, words):
         if not words:
             return ""
-        command_name = words[0]
-        arguments = words[1:]
-        command = self.find_command(command_name)
-        if command is None:
-            return f"Unknown command: {command_name}"
+        command_name, arguments = words[0], words[1:]
+        command = lookup_command(self.bindings, command_name)
+        return apply_call(context, command, command_name, arguments)
+
+    def register_command(self, name, fn, varargs=False, regex=False, contextual=False):
+        if regex:
+            name = re.compile("^" + name + "$")
+        self.bindings.append(CommandBinding(name, fn, varargs, regex, contextual))
+
+    def command_display_name(self, command):
+        if command.regex:
+            return f"/{command.command_name.pattern[1:-1]}/"
+        else:
+            return command.command_name
+
+    def command_usage(self, command):
+        result = self.command_display_name(command)
+        parameters = list(signature(command.fn).parameters.values())
         if command.contextual:
-            context = dict(context)
-            context['command'] = command_name
-            arguments.insert(0, context)
-        if command.varargs:
-            last_arg_index = self.max_args(command.fn) - 1
-            first_args = arguments[:last_arg_index]
-            last_args = arguments[last_arg_index:]
-            if last_args:
-                arguments = first_args + [" ".join(last_args)]
+            # pop context argument
+            parameters.pop(0)
+        for i, parameter in enumerate(parameters):
+            if i == len(parameters) - 1 and command.varargs:
+                result += f" {{{parameter.name}...}}"
+            elif parameter.default != Parameter.empty:
+                result += f" [{parameter.name}]"
             else:
-                arguments = first_args
-        if not self.signature_accepts_arguments(command.fn, arguments):
-            return f"Usage: {command.usage}"
-        return command.fn(*arguments)
-
-    def find_command(self, command_name):
-        for command in self.commands:
-            if command.regex:
-                if command.name.match(command_name):
-                    return command
-            else:
-                if command.name == command_name:
-                    return command
-        return None
-
-    def max_args(self, fn):
-        sig = signature(fn)
-        max_args = 0
-        for parameter in sig.parameters.values():
-            if parameter.kind in [Parameter.POSITIONAL_ONLY,
-                                  Parameter.POSITIONAL_OR_KEYWORD]:
-                max_args += 1
-        return max_args
-
-    def signature_accepts_arguments(self, fn, arguments):
-        try:
-            sig = signature(fn)
-            sig.bind(*arguments)
-            return True
-        except TypeError:
-            return False
+                result += f" <{parameter.name}>"
+        return result
 
     def help(self, command_name=None):
         if not command_name:
             result = "Available commands: "
-            result += ", ".join(command.display_name for command in self.commands)
+            result += ", ".join(self.command_display_name(command) for command in self.bindings)
             return result
         else:
-            command = self.find_command(command_name)
+            command = lookup_command(self.bindings, command_name)
             if command:
-                return f"Usage: {command.usage}"
+                return "Usage: {}".format(self.command_usage(command))
             else:
                 return f"Unknown command: {command_name}"
 
