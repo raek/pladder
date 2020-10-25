03586bc71896686c5edb5ce54222c9ff172a4eea
Rasmus Bondesson <raek@raek.se>
2020-10-25 21:03:59 +0100

    Add more information to version string

--- upgrade.sh
+++ upgrade.sh
@@ -11,7 +11,7 @@ fi
 git fetch --prune origin
 git merge --ff-only origin/master
 pip3 uninstall -y pladder
-echo "LAST_COMMIT=\"$(git log --pretty=format:"%h %s" -n 1)\"" > pladder/__init__.py
+echo "LAST_COMMIT=\"$(git log --pretty=format:"%h \"%s\" by %an, %ad" -n 1)\"" > pladder/__init__.py
 pip3 install --user --no-warn-script-location "$1"
 git checkout -- pladder/__init__.py
 systemctl --user daemon-reload
