bd2e463673918c30c996c3f772898f95ff3cccae
Rasmus Bondesson <raek@raek.se>
2020-11-28 09:13:14 +0100

    Handle force pushes better

--- upgrade.sh
+++ upgrade.sh
@@ -9,7 +9,7 @@ if [[ $# != 1 ]]; then
 fi
 
 git fetch --prune origin
-git merge --ff-only origin/master
+git reset --hard origin/master
 pip3 uninstall -y pladder
 echo "LAST_COMMIT=$(git log --pretty=format:"%h \"%s\" by %an, %ad" -n 1 | python3 -c "import sys; sys.stdout.write(repr(next(sys.stdin)))")" > pladder/__init__.py
 pip3 install --user --no-warn-script-location "$1"
