205ffdcd971ebb9bf75fb3a69c71b7b29aadd900
Janne Virsunen <janne.virsunen@gmail.com>
2020-12-06 21:58:39 +0100

    Re-added pip3 install requirements, but with --user this time

--- upgrade.sh
+++ upgrade.sh
@@ -12,7 +12,7 @@ git fetch --prune origin
 git reset --hard origin/master
 pip3 uninstall -y pladder
 echo "LAST_COMMIT=$(git log --pretty=format:"%h \"%s\" by %an, %ad" -n 1 | python3 -c "import sys; sys.stdout.write(repr(next(sys.stdin)))")" > pladder/__init__.py
-#pip3 install -r requirements.txt --user
+pip3 install -r requirements.txt --user
 pip3 install --user --no-warn-script-location "$1"
 git checkout -- pladder/__init__.py
 systemctl --user daemon-reload
