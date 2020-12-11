791806e824d381a1ce2111bbf3cc217ec75f192d
Rasmus Bondesson <raek@raek.se>
2020-12-11 21:52:10 +0100

    Remove varargs, which hid the argument help from the user

--- pladder/alias.py
+++ pladder/alias.py
@@ -16,13 +16,12 @@ class AliasPlugin(Plugin):
         self.alias_db = self.enter_context(AliasDb(alias_db_path))
         self.bot = bot
         bot.register_command("alias", self.help)
-        bot.register_command("add-alias", self.add_alias, varargs=True)
-        bot.register_command("get-alias", self.alias_db.get_alias, varargs=True)
-        bot.register_command("del-alias", self.del_alias, varargs=True)
-        bot.register_command("list-alias", self.alias_db.list_alias, varargs=True)
-        bot.register_command("random-alias", self.alias_db.random_alias, varargs=True)
+        bot.register_command("add-alias", self.add_alias)
+        bot.register_command("get-alias", self.alias_db.get_alias)
+        bot.register_command("del-alias", self.del_alias)
+        bot.register_command("list-alias", self.alias_db.list_alias)
+        bot.register_command("random-alias", self.alias_db.random_alias)
         self.register_db_bindings()
-
 
     def help(self):
         return "Functions: get-alias [name], del-alias [name], add-alias [name] [content], list-alias *[name]*, random-alias *[name]*. Wildcards are % and _. Use {} when adding PladderScript to database."
@@ -54,25 +53,21 @@ class AliasPlugin(Plugin):
                     return True
         return False
     
-    def add_alias(self, args):
-        try:
-            name, data = args.split(" ", 1)
-        except:
-            return "Det lär ju inte bara vara ett ord ditt mähulkande våp! ~add-alias hest {[prefix] gillar hest!}"
+    def add_alias(self, name, data):
         if self.binding_exists(name):
             return "Hallå farfar, den finns ju redan."
         if result := self.alias_db.add_alias(name, data):
             self.register_binding(name)
         return result
     
-    def del_alias(self, args):
-        if self.binding_exists(args):
+    def del_alias(self, name):
+        if self.binding_exists(name):
             try: 
-                result = self.alias_db.del_alias(args)
+                result = self.alias_db.del_alias(name)
             except:
                 return "Det blir inget med det."
             else:
-                self.remove_binding(args)
+                self.remove_binding(name)
             return result
         else:
             return "Nej"
@@ -146,11 +141,11 @@ class AliasDb(ExitStack):
             raise DBError("Om du ser det här har Krille fuckat upp")
         return self._insert_alias(name, data)
     
-    def get_alias(self, args):
-        if self._alias_exists(args):
+    def get_alias(self, name):
+        if self._alias_exists(name):
             c = self._db.cursor()
             try:
-                c.execute("SELECT name, data FROM alias WHERE name=?", [args])
+                c.execute("SELECT name, data FROM alias WHERE name=?", [name])
             except:
                 return "eror :("
             else:
@@ -159,12 +154,12 @@ class AliasDb(ExitStack):
         else:
             return "Nej"
     
-    def del_alias(self, args):
-        if self._alias_exists(args):
+    def del_alias(self, name):
+        if self._alias_exists(name):
             c = self._db.cursor()
             c.execute("BEGIN TRANSACTION")
             try:
-                c.execute("DELETE FROM alias WHERE name=?", [args])
+                c.execute("DELETE FROM alias WHERE name=?", [name])
             except:
                 self._db.rollback()
                 raise DBError("You cannot delete ye flask")
@@ -174,9 +169,9 @@ class AliasDb(ExitStack):
         else:
             raise DBError("poop")
     
-    def list_alias(self, args):
+    def list_alias(self, name_pattern):
         c = self._db.cursor()
-        searchstr = "%"+args+"%"
+        searchstr = "%"+name_pattern+"%"
         try:
             c.execute("SELECT name FROM alias WHERE name LIKE ?", [searchstr])
         except:
@@ -189,8 +184,8 @@ class AliasDb(ExitStack):
             result = result.strip()
             return result
     
-    def random_alias(self, args):
-        list = self.list_alias(args)
+    def random_alias(self, name_pattern):
+        list = self.list_alias(name_pattern)
         return random.choice(list.split())
 
 
