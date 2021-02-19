31087cfac8e18ebf82af4e9a78082e3493320b21
Rasmus Bondesson <raek@raek.se>
2021-02-19 14:44:54 +0100

    Make check.sh slightly more chatty

--- check.sh
+++ check.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-python3 -m mypy -p pladder
-python3 -m flake8
-python3 -m pytest
+echo "Mypy (type checking)"
+python3 -m mypy -p pladder && echo OK
+echo "Flake8"
+python3 -m flake8 && echo OK
+echo "Pytest"
+python3 -m pytest && echo OK
