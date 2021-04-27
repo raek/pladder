cb31ee9ac5a2bf4140b18df83feaec6e6a23575c
Rasmus Bondesson <raek@raek.se>
2021-04-27 13:17:21 +0200

    Add vrålify unit tests

---
+++ pladder/test_misc.py
@@ -0,0 +1,59 @@
+from typing import NamedTuple
+
+from pladder.misc import vraaaal
+
+
+class DummyContext(NamedTuple):
+    command_name: str
+
+
+def run(command_name, argument):
+    return vraaaal(DummyContext(command_name), argument)
+
+
+def test_one_aa_short_word():
+    assert run("vrålify", "hej") == "HEJ"
+
+
+def test_two_aa_short_word():
+    assert run("vråålify", "hej") == "HEEEEEJ"
+
+
+def test_three_aa_short_word():
+    assert run("vrååålify", "hej") == "HEEEEEEEEEJ"
+
+
+def test_one_aa_medium_word():
+    assert run("vrålify", "bananarama") == "BANANARAMA"
+
+
+def test_two_aa_medium_word():
+    assert run("vråålify", "bananarama") == "BAAANAAANAAARAAAMAAA"
+
+
+def test_three_aa_medium_word():
+    assert run("vrååålify", "bananarama") == "BAAAAANAAAAANAAAAARAAAAAMAAAAA"
+
+
+def test_one_aa_long_word():
+    assert run("vrålify", "bananaramabandana") == "BANANARAMABANDANA"
+
+
+def test_two_aa_long_word():
+    assert run("vråålify", "bananaramabandana") == "BAANAANAARAAMAABAANDAANAA"
+
+
+def test_three_aa_long_word():
+    assert run("vrååålify", "bananaramabandana") == "BAAANAAANAAARAAAMAAABAAANDAAANAAA"
+
+
+def test_one_aa_hng():
+    assert run("vrålify", "hng") == "HNG"
+
+
+def test_two_aa_hng():
+    assert run("vråålify", "hng") == "HNNNNNG"
+
+
+def test_three_aa_hng():
+    assert run("vrååålify", "hng") == "HNNNNNNNNNG"
