b558ce619ea261805110f8c55d896bc76030df49
Rasmus Bondesson <raek@raek.se>
2021-05-13 15:31:37 +0200

    pladder.irc.dbus: Whitespace fixes

--- pladder/irc/dbus.py
+++ pladder/irc/dbus.py
@@ -18,15 +18,15 @@ class DbusHook(Hook):
 
     def on_trigger(self, timestamp, channel, sender, text):
         return self.bot.RunCommand(timestamp, self.config.network, channel, sender.nick, text,
-                                    on_error=self._handle_bot_error)
+                                   on_error=self._handle_bot_error)
 
     def on_privmsg(self, timestamp, channel, sender, text):
         self.log.AddLine(timestamp, self.config.network, channel, sender.nick, text,
-                          on_error=self._handle_log_error)
+                         on_error=self._handle_log_error)
 
     def on_send_privmsg(self, timestamp, channel, nick, text):
         self.log.AddLine(timestamp, self.config.network, channel, nick, text,
-                          on_error=self._handle_log_error)
+                         on_error=self._handle_log_error)
 
     def _handle_bot_error(self, e):
         if "org.freedesktop.DBus.Error.ServiceUnknown" in str(e):
