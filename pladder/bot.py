e3c2167189759aa942fbe052b4b6f1a80b090725
Rasmus Bondesson <raek@raek.se>
2021-05-14 16:53:37 +0200

    Make 'channels' command default to current network

--- pladder/bot.py
+++ pladder/bot.py
@@ -45,7 +45,7 @@ class PladderBot(ExitStack):
         self.register_command("version", self.version)
         self.register_command("searchlog", self.searchlog, contextual=True)
         self.register_command("send", self.send, contextual=True, varargs=True)
-        self.register_command("channels", self.channels)
+        self.register_command("channels", self.channels, contextual=True)
         self.register_command("comp", self.comp, contextual=True)
         self.register_command("give", self.give, varargs=True)
         self.register_command("echo", lambda text="": text, varargs=True)
@@ -196,13 +196,15 @@ class PladderBot(ExitStack):
         else:
             return "Message sent."
 
-    def channels(self, network):
+    def channels(self, context, network=None):
+        if network is None:
+            network = context.metadata["network"]
         connector = RetryProxy(self.bus, f"se.raek.PladderConnector.{network}")
         channels = connector.GetChannels(on_error=lambda e: None)
         if channels is None:
             return f"Not connected to network {network}."
         else:
-            return ", ".join(sorted(channels))
+            return f"{network}: {', '.join(sorted(channels))}"
 
     def show_context(self, context):
         return repr({
