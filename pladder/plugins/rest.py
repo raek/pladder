a5246aef7f09b1139dd03899fe698a0b71c7b3f7
Rasmus Bondesson <raek@raek.se>
2021-10-19 22:12:55 +0200

    rest: Fix Flake8 warnings

--- pladder/plugins/rest.py
+++ pladder/plugins/rest.py
@@ -1,12 +1,16 @@
 from contextlib import contextmanager
+
 from pladder.script.types import ScriptError
+
 import requests
+
 
 @contextmanager
 def pladder_plugin(bot):
     cmds = bot.new_command_group("rest")
     cmds.register_command("rest-post-simple", rest_post_simple)
     yield
+
 
 def rest_post_simple(url, message):
     """
@@ -19,4 +23,3 @@ def rest_post_simple(url, message):
         raise ScriptError("Unexpected error code: %d" % r.status_code)
     else:
         return r.text
-
